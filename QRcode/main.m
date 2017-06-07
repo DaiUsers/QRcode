f54656fcc49e59036cd323764ff8ca99d79a76ba
王恒 <wanghengbai@126.com>
2017-06-07 17:42:43 +0800

    Initial Commit

---
+++ QRcode/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  QRcode
+//
+//  Created by wheng on 17/6/7.
+//  Copyright © 2017年 admin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
