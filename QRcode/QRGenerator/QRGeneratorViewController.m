69236503e24d372d351dfcba9846be075af64518
wheng <wanghengbai@126.com>
2017-06-08 22:11:45 +0800

    feat：QRGenerator

--- QRcode/QRGenerator/QRGeneratorViewController.m
+++ QRcode/QRGenerator/QRGeneratorViewController.m
@@ -17,6 +17,42 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    
+    UIImageView *imageView = [[UIImageView alloc] initWithFrame:CGRectMake(self.view.center.x - 50, 100, 100, 100)];
+    [self.view addSubview:imageView];
+    
+    imageView.image = [self qrImageWithString:@"https://github.com/DaiUsers/QRcode" size:100];
+}
+
+- (UIImage *)qrImageWithString:(NSString *)string size:(CGFloat)size {
+    CIImage *qrCIImage = [self qrCIImageFromeString:string];
+    
+    CGImageRef cgImage = [[CIContext contextWithOptions:nil] createCGImage:qrCIImage fromRect:qrCIImage.extent];
+    UIGraphicsBeginImageContext(CGSizeMake(size, size));
+    CGContextRef contextRef = UIGraphicsGetCurrentContext();
+    CGContextSetInterpolationQuality(contextRef, kCGInterpolationNone);
+    CGContextScaleCTM(contextRef, 1.0, -1.0);
+    CGContextDrawImage(contextRef, CGContextGetClipBoundingBox(contextRef), cgImage);
+    UIImage *qrImage = UIGraphicsGetImageFromCurrentImageContext();
+    UIGraphicsEndImageContext();
+    CGImageRelease(cgImage);
+    return qrImage;
+}
+
+- (CIImage *)qrCIImageFromeString:(NSString *)string {
+    NSData *stringData = [string dataUsingEncoding:NSUTF8StringEncoding];
+    //滤镜，类型->CIQRCodeGenerator
+    CIFilter *qrfilter = [CIFilter filterWithName:@"CIQRCodeGenerator"];
+    [qrfilter setValue:stringData forKey:@"inputMessage"];
+    //错误修正容量 L(7%)/M(15%)/Q(25%)/H(30%)
+    [qrfilter setValue:@"M" forKey:@"inputCorrectionLevel"];
+    
+    UIColor *onColor  = [UIColor blackColor];
+    UIColor *offColor = [UIColor whiteColor];
+    
+    CIFilter *colorFilter = [CIFilter filterWithName:@"CIFalseColor" keysAndValues:@"inputImage",qrfilter.outputImage,@"inputColor0",[CIColor colorWithCGColor:onColor.CGColor],@"inputColor1",[CIColor colorWithCGColor:offColor.CGColor], nil];
+    
+    return colorFilter.outputImage;
 }
 
 - (void)didReceiveMemoryWarning {
