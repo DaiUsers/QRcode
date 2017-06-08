84e9538fa6107343a54da087615f6ac79b840b42
王恒 <wanghengbai@126.com>
2017-06-08 15:12:40 +0800

    feat：add specific area

--- QRcode/QRScanner/QRScannerViewController.m
+++ QRcode/QRScanner/QRScannerViewController.m
@@ -26,14 +26,22 @@
     // Do any additional setup after loading the view.
     
     [self initDevice];
+    [self addBackGroundView];
 }
 
 - (void)initDevice {
+    //摄像设配
     self.device = [AVCaptureDevice defaultDeviceWithMediaType:AVMediaTypeVideo];
+    //输入流
     self.input  = [AVCaptureDeviceInput deviceInputWithDevice:self.device error:nil];
+   //输出流
     self.output = [[AVCaptureMetadataOutput alloc] init];
+    //设置代理 主线程调用
     [self.output setMetadataObjectsDelegate:self queue:dispatch_get_main_queue()];
+    
+    //连接
     self.session = [[AVCaptureSession alloc] init];
+    //分辨度高
     [self.session setSessionPreset:AVCaptureSessionPresetHigh];
     
     if ([self.session canAddInput:self.input]) {
@@ -42,17 +50,42 @@
     if ([self.session canAddOutput:self.output]) {
         [self.session addOutput:self.output];
     }
-    
+    //输出流支持的对象格式（QRCode）
     [self.output setMetadataObjectTypes:@[AVMetadataObjectTypeQRCode]];
     
+    //实时预览层
     self.previewlayer = [AVCaptureVideoPreviewLayer layerWithSession:self.session];
+    //AVLayerVideoGravityResizeAspectFill （Preserve aspect ratio; fill layer bounds）
+    //AVLayerVideoGravityResizeAspect      (Preserve aspect ratio; fit within layer bounds)
     [self.previewlayer setVideoGravity:AVLayerVideoGravityResizeAspectFill];
     
-    [self.previewlayer setFrame:self.view.layer.bounds];
+    [self.previewlayer setFrame:self.view.bounds];
     
-    [self.view.layer insertSublayer:self.previewlayer atIndex:0];
-    
+    [self.view.layer addSublayer:self.previewlayer];
     [self.session startRunning];
+
+    CGRect rect         = CGRectMake(self.view.center.x - 100, 100, 200, 200);
+    //it work after session.startRunning (transform rect)
+    CGRect interRect    = [self.previewlayer metadataOutputRectOfInterestForRect:rect];
+    
+    self.output.rectOfInterest = interRect;
+    
+    
+}
+
+- (void)addBackGroundView {
+    UIView *frameView = [[UIView alloc] initWithFrame:self.view.bounds];
+    [self.view addSubview:frameView];
+    
+    frameView.backgroundColor = [UIColor colorWithRed:0 green:0 blue:0 alpha:0.5];
+    UIBezierPath *maskPath = [UIBezierPath bezierPathWithRect:self.view.bounds];
+    UIBezierPath *centerPath = [[UIBezierPath bezierPathWithRoundedRect:CGRectMake(30, 100, self.view.bounds.size.width - 60, 300) cornerRadius:2.0] bezierPathByReversingPath];;
+    [maskPath appendPath:centerPath];
+    
+    CAShapeLayer *maskLayer = [[CAShapeLayer alloc] init];
+    maskLayer.path = maskPath.CGPath;
+
+    frameView.layer.mask = maskLayer;
 }
 
 #pragma mark AVCaptureMetadataOutputObjectsDelegate
